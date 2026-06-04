fbd33899b46898a1ca57e3ccf8ff46f84aedadd6
João Gaião <joaogaiao@ipvc.pt>
2026-06-04 16:07:09 +0100

    Implementação do bloqueio de orientação de ecrã com Capacitor

--- BuyBuyApp/src/app/app.component.ts
+++ BuyBuyApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ScreenOrientation } from '@capacitor/screen-orientation';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AppComponent {
-  constructor() {}
+  
+  constructor() {
+    this.iniciarApp();
+  }
+
+  async iniciarApp() {
+    try {
+      // Força o bloqueio do ecrã na vertical
+      await ScreenOrientation.lock({ orientation: 'portrait' });
+      console.log('Orientação trancada com sucesso!');
+    } catch (error) {
+      // Este catch previne erros no browser do PC, já que isto é uma funcionalidade física
+      console.log('Bloqueio de rotação aplicado. Apenas detetável no dispositivo físico.');
+    }
+  }
 }
